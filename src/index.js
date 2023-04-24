ff372c7acf795a806b2b92db59ebe58e86d1a82a
wit <witwitchayakarn@gmail.com>
2023-04-24 11:25:22 +0700

    Remove unused redirect naked domain name to www

--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import express from 'express';
 
-import { isString } from './utils';
-
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  const host = req.get('host');
-  if (isString(host) && host.toLowerCase().startsWith('stacksdrive.com')) {
-    return res.redirect(302, `https://www.${host}${req.url}`);
-  }
-  return next();
-});
 
 app.get('/', (_req, res) => {
   res.send('Welcome to <a href="https://www.stacksdrive.com">Stacks Drive</a>\'s server!');
